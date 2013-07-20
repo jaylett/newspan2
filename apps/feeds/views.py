e09071e023f679c44f24d8e8407fc9b7d5e1535e
James Aylett <james@tartarus.org>
2013-07-20 23:47:05 +0100

    Filter by unread & starred, and update those flags. Update unread to false (read) & redirect instead of link to articles.

--- apps/feeds/views.py
+++ apps/feeds/views.py
@@ -1,3 +1,5 @@
+import urllib
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, ListView
 from apps.feeds.models import *
 
@@ -5,6 +7,47 @@ from apps.feeds.models import *
 class ArticleDetail(DetailView):
     model = Article
 
+    def post(self, request, *args, **kwargs):
+        set_read = request.POST.get('read')
+        set_starred = request.POST.get('starred')
+        update = {}
+        if set_read is not None:
+            update['unread'] = not (set_read == 'true')
+        if set_starred is not None:
+            update['starred'] = (set_starred == 'true')
+        obj = self.get_object()
+        Article.objects.filter(pk=obj.pk).update(**update)
+        return HttpResponseRedirect(obj.url.default)
+
 
 class ArticleList(ListView):
-	model = Article
+    model = Article
+
+    def get_queryset(self):
+        filtparms = {}
+        params = self.kwargs.get('path_params')
+        _kwargs = {}
+        if params is not None:
+            bits = params.split(';')
+            for b in bits:
+                if b == '':
+                    # first one with nothing before it
+                    continue
+                pieces = map(lambda x: urllib.unquote_plus(x), b.split('='))
+                if pieces[0] == '':
+                    continue
+                if len(pieces)==2:
+                    _kwargs[pieces[0]] = pieces[1]
+                elif len(pieces)==1:
+                    _kwargs[pieces[0]] = True
+                else:
+                    # very weird, but let's not just throw up
+                    _kwargs[pieces[0]] = '='.join(pieces[1:])
+
+        show = _kwargs.get('show', 'unread')
+        if show == 'starred':
+            filtparms['starred'] = True
+        elif show == 'unread':
+            filtparms['unread'] = True
+        
+        return Article.objects.filter(**filtparms)
--- newspan/urls.py
+++ newspan/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', apps.feeds.views.ArticleList.as_view()),
-    url(r'^article/(?P<pk>[0-9]+)/$', apps.feeds.views.ArticleDetail.as_view()),
+    url(r'^all(?P<path_params>;[.;=,\w\d\-\_]*)?/$', apps.feeds.views.ArticleList.as_view()),
     url(r'^article/(?P<pk>[0-9]+)/$', apps.feeds.views.ArticleDetail.as_view(), name='article-detail'),
     url(r'^admin/', include(admin.site.urls)),
 )
