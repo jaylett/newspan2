da86982d40526f784295a2d2a5f74046bf675d3a
James Aylett <james@tartarus.org>
2013-07-20 15:24:49 +0100

    Postgresql, south, CAPE, sass & a simple article template.

--- newspan/settings.py
+++ newspan/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,12 @@ WSGI_APPLICATION = 'newspan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newspan',
+        'USERS': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
@@ -80,3 +85,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
--- newspan/urls.py
+++ newspan/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'newspan.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name='article.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
