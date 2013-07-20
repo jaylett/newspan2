2574ad23c2489861bcb412198e50a9ef17768f3f
James Aylett <james@tartarus.org>
2013-07-20 18:51:17 +0100

    Typo in settings.

--- newspan/settings.py
+++ newspan/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'newspan',
-        'USERS': '',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '',
